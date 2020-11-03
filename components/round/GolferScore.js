d311ea6f79806a82dc305eeee9ca7fd24c94955c
conmart <connor.a.martinelli@gmail.com>
2020-11-03 12:34:55 -0800

    style score modifiers

--- components/round/GolferScore.js
+++ components/round/GolferScore.js
@@ -2,11 +2,39 @@ import { Box } from '@chakra-ui/core';
 
 export default function GolferScore({ par, score, editHole }) {
   const diff = score - par;
-  console.log(diff, 'diff')
+
+  const calcScoreModifier = () => {
+    const absDiff = Math.abs(diff);
+    let class1 = 'score-sym';
+    let class2 = '';
+    
+    if (absDiff > 1) {
+      class2 = 'score-sym small-sym';
+      if (absDiff > 2) {
+        class1 += ' solid-border'
+      }
+    }
+    if (diff < 0) {
+      class1 += ' circle';
+      class2 += ' circle';
+    }
+
+    return [class1, class2]
+  };
+
+  const [spanClass1, spanClass2] = calcScoreModifier();
 
   return (
     <td onClick={editHole}>
-      <Box cursor="pointer">{score}</Box>
+      <Box cursor="pointer" position="relative">
+        {!!diff && (
+          <>
+            <span className={spanClass1} />
+            <span className={spanClass2} />
+          </>
+        )}
+        {score}
+      </Box>
     </td>
   );
 }
